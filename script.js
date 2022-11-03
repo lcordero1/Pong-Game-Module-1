6b9a0fcb735e71fb3869a53aa3f292e3354cb0c6
Lariana C01~ <cordero.lariana@gmail.com>
2022-11-03 10:56:07 -0400

    Edits JS,CSS

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 const canvas =  document.querySelector('canvas')
 const context = document.querySelector('2d')
+const compScore = document.getElementById("computer-score")
+const mainPlScore = document.getElementById("mainPlayer-score")
 
+function drawRect(color, x, y, w, h) {
+    ctx1.fillStyle = "white"; //color
+//  ctx.fillRect(x, y, width, height)
+    ctx.fillRect(50, 50 , 25, 100);
+}
+function drawCircle() {
+    ctx.fillStyle = "white"
+    ctx.beginPath()
+// ctx.arc(x, y, radius, start angle (Math.PI*2), direction(not really important for circle))
+    ctx.arc(50, 50, Math.PI*2, false )
+    ctx.closePath()
+    ctx.fillShape()
+
+}
+function collisionDetection()
+function framePerSecond()
+
+const mainPlayer = document.querySelector("main-player") 
+
+
+const computerOpp = document.querySelector("computer")
+
+
+
+const ball = {
+
+}
