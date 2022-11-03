cfda13abb92d1ff472d13d819c3843fd398733a3
Lariana C01~ <cordero.lariana@gmail.com>
2022-11-03 15:16:21 -0400

    JS edit

--- script.js
+++ script.js
@@ -26,17 +26,18 @@ function drawRect(xAxis, yAxis, width, height, color) {
     ctx.fillStyle = color;
     ctx.fillRect(xAxis, yAxis, width, height);
 }
-drawRect(50, 100, 25, 50, "white")
+drawRect(0, 100, 15, 50, "white")
+drawRect(0, -100, -15, -50, "white")
 
-function drawCircle() {
-    ctx.fillStyle = "white"
+function drawCircle(xAxis, yAxis, radius, color) {
+    ctx.fillStyle = color,
     ctx.beginPath()
-    // ctx.arc(x, y, radius, start angle (Math.PI*2), direction(not really important for circle))
-    ctx.arc(0, 0, Math.PI * 2, false)
+    ctx.arc(xAxis, yAxis, radius, color)
     ctx.closePath()
     ctx.fillShape()
 
 }
+
 function moveBallRand(ball) {
     ball.style.position = 'absolute';
     ball.style.top = Math.floor(Math.random() * 90 + 5) + '%';
